79a6b513d7e84418aaaf414efcf7cfab21149294
khubaib004 <khabbab607342@gmail.com>
2023-09-15 16:01:02 +0600

    create khowledge project with tailwind-CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+
+            <h1 className='text-6xl bg-red-300 text-center'>Knowledge cafe</h1>
+
+        </>
+    )
+}
+
+export default App
